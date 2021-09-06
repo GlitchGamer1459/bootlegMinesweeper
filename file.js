29e6f05329e136bd47a16985ac3741648bec6ddd
GlitchGamer1459 <glitchgamer1459@gmail.com>
2021-09-06 14:32:17 -0700

    added boardSize prompt

--- file.js
+++ file.js
@@ -1,4 +1,4 @@
-var gridSize = 20;
+var boardSize;
 var gridBuilt = false;
 var w;
 var x;
@@ -6,21 +6,25 @@ var y = 0;
 var z;
 var t;
 
+function onLoad() {
+    boardSize = prompt("enter board size as whole # between 10 & 20");
+}
+
 function buildGrid() {
     if (gridBuilt === false) {
         var w = document.createElement("TABLE");
         w.setAttribute("id", "myTable");
         document.body.appendChild(w);
 
-        for (var i = 0; i < gridSize; i++) {
+        for (var i = 0; i < boardSize; i++) {
             x = document.createElement("TR");
             x.setAttribute("id", "myTr" + i);
             document.getElementById("myTable").appendChild(x);
 
         }
 
-        for (var i = 0; i < gridSize; i++) {
-            for (var o = 0; o < gridSize; o++) {
+        for (var i = 0; i < boardSize; i++) {
+            for (var o = 0; o < boardSize; o++) {
                 y = y + 1;
                 z = document.createElement("TD");
                 t = document.createTextNode("cell" + y);

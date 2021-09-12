06d82d63b06cb5f16fee855328839eb566366fd0
GlitchGamer1459 <glitchgamer1459@gmail.com>
2021-09-11 22:29:12 -0700

    idk man

--- file.js
+++ file.js
@@ -47,119 +47,118 @@ function onLoad() {
     }
 }
 
-//builds the visible board in index.html
-function buildGrid() {
-    if (gridBuilt === false) {
-        var w = document.createElement("TABLE");
-        w.setAttribute("id", "myTable");
-        document.body.appendChild(w);
-
-        for (var i = 0; i < boardSize; i++) {
-            x = document.createElement("TR");
-            x.setAttribute("id", "myTr" + i);
-            document.getElementById("myTable").appendChild(x);
-
-        }
-
-        for (var i = 0; i < boardSize; i++) {
-            for (var o = 0; o < boardSize; o++) {
-                y = y + 1;
-                z = document.createElement("TD");
-                z.setAttribute("id", y);
-                z.addEventListener("click", function () {
-                    m = this.id;
-                    onClick(m);
-                });
-                t = document.createTextNode('');
-                z.appendChild(t);
-                document.getElementById("myTr" + o).appendChild(z);
-            }
-        }
-    }
-    gridBuilt = true;
-}
-
 //begin the game function
 function startGame() {
     buildGrid();
-}
 
-//generates a 2d array of cell objects within <global>
-  //create subarrays
-  for (var i = 0; i < boardSize; i++) {
-      board.push([]);
-}
+    //builds the visible board in index.html
+    function buildGrid() {
+        if (gridBuilt === false) {
+            var w = document.createElement("TABLE");
+            w.setAttribute("id", "myTable");
+            document.body.appendChild(w);
 
-  //generate cellX objects
-  for (var iteration = 0; iteration < boardSize; iteration++) {
-      for (var o = 0; o < boardSize; o++) {
-          a = a + 1;
-          eval('var cell' + a + ' = {tileValue:0, tileState:0, tileID:' + a + '}');
-          board[iteration][o] = eval('cell' + a);
-      }
-  }
+            for (var i = 0; i < boardSize; i++) {
+                x = document.createElement("TR");
+                x.setAttribute("id", "myTr" + i);
+                document.getElementById("myTable").appendChild(x);
 
-  //place mines with edge occlusion
-  for (var i = 0; i < mineNumber; i++) {
-      h = Math.floor((Math.random() * (boardSize - 2)) + 1);
-      j = Math.floor((Math.random() * (boardSize - 2)) + 1);
-      b = board[h][j];
-      if (eval('cell' + b.tileID + '.tileValue != 9')) {
-          eval('cell' + b.tileID + '.tileValue = 9;');
-      }
-  }
+            }
 
-  //set adjacent tile values
-  for (var i = 0; i < boardSize; i++) {
-      for (var o = 0; o < boardSize; o++) {
-          c = board[i][o];
-          if (c.tileValue === 9) {
-              console.log("found");
-              d = i;
-              e = o;
-              d = d - 1;
-              f = board[d][e];
-              if (eval('cell' + f.tileID + '.tileValue') != 9) {
-                  eval('cell' + f.tileID + '.tileValue += 1');
-              }
-              e = e - 1;
-              f = board[d][e];
-              if (eval('cell' + f.tileID + '.tileValue') != 9) {
-                  eval('cell' + f.tileID + '.tileValue += 1');
-              }
-              d = d + 1;
-              f = board[d][e];
-              if (eval('cell' + f.tileID + '.tileValue') != 9) {
-                  eval('cell' + f.tileID + '.tileValue += 1');
-              }
-              d = d + 1;
-              f = board[d][e];
-              if (eval('cell' + f.tileID + '.tileValue') != 9) {
-                  eval('cell' + f.tileID + '.tileValue += 1');
-              }
-              e = e + 1;
-              f = board[d][e];
-              if (eval('cell' + f.tileID + '.tileValue') != 9) {
-                  eval('cell' + f.tileID + '.tileValue += 1');
-              }
-              e = e + 1;
-              f = board[d][e];
-              if (eval('cell' + f.tileID + '.tileValue') != 9) {
-                  eval('cell' + f.tileID + '.tileValue += 1');
-              }
-              d = d - 1;
-              f = board[d][e];
-              if (eval('cell' + f.tileID + '.tileValue') != 9) {
-                  eval('cell' + f.tileID + '.tileValue += 1');
-              }
-              d = d - 1;
-              f = board[d][e];
-              if (eval('cell' + f.tileID + '.tileValue') != 9) {
-                  eval('cell' + f.tileID + '.tileValue += 1');
-              }
-          }
-      }
-  }
+            for (var i = 0; i < boardSize; i++) {
+                for (var o = 0; o < boardSize; o++) {
+                    y = y + 1;
+                    z = document.createElement("TD");
+                    z.setAttribute("id", y);
+                    z.addEventListener("click", function () {
+                        m = this.id;
+                        onClick(m);
+                    });
+                    t = document.createTextNode('');
+                    z.appendChild(t);
+                    document.getElementById("myTr" + o).appendChild(z);
+                }
+            }
+        }
+        gridBuilt = true;
+    }
+
+    //generates a 2d array of cell objects within <global>
+    //create subarrays
+    for (var i = 0; i < boardSize; i++) {
+        board.push([]);
+    }
+
+    //generate cellX objects
+    for (var iteration = 0; iteration < boardSize; iteration++) {
+        for (var o = 0; o < boardSize; o++) {
+            a = a + 1;
+            eval('var cell' + a + ' = {tileValue:0, tileState:0, tileID:' + a + '}');
+            board[iteration][o] = eval('cell' + a);
+        }
+    }
+
+    //place mines with edge occlusion
+    for (var i = 0; i < mineNumber; i++) {
+        h = Math.floor((Math.random() * (boardSize - 2)) + 1);
+        j = Math.floor((Math.random() * (boardSize - 2)) + 1);
+        b = board[h][j];
+        if (eval('cell' + b.tileID + '.tileValue != 9')) {
+            eval('cell' + b.tileID + '.tileValue = 9;');
+        }
+    }
+
+    //set adjacent tile values
+    for (var i = 0; i < boardSize; i++) {
+        for (var o = 0; o < boardSize; o++) {
+            c = board[i][o];
+            if (c.tileValue === 9) {
+                console.log("found");
+                d = i;
+                e = o;
+                d = d - 1;
+                f = board[d][e];
+                if (eval('cell' + f.tileID + '.tileValue') != 9) {
+                    eval('cell' + f.tileID + '.tileValue += 1');
+                }
+                e = e - 1;
+                f = board[d][e];
+                if (eval('cell' + f.tileID + '.tileValue') != 9) {
+                    eval('cell' + f.tileID + '.tileValue += 1');
+                }
+                d = d + 1;
+                f = board[d][e];
+                if (eval('cell' + f.tileID + '.tileValue') != 9) {
+                    eval('cell' + f.tileID + '.tileValue += 1');
+                }
+                d = d + 1;
+                f = board[d][e];
+                if (eval('cell' + f.tileID + '.tileValue') != 9) {
+                    eval('cell' + f.tileID + '.tileValue += 1');
+                }
+                e = e + 1;
+                f = board[d][e];
+                if (eval('cell' + f.tileID + '.tileValue') != 9) {
+                    eval('cell' + f.tileID + '.tileValue += 1');
+                }
+                e = e + 1;
+                f = board[d][e];
+                if (eval('cell' + f.tileID + '.tileValue') != 9) {
+                    eval('cell' + f.tileID + '.tileValue += 1');
+                }
+                d = d - 1;
+                f = board[d][e];
+                if (eval('cell' + f.tileID + '.tileValue') != 9) {
+                    eval('cell' + f.tileID + '.tileValue += 1');
+                }
+                d = d - 1;
+                f = board[d][e];
+                if (eval('cell' + f.tileID + '.tileValue') != 9) {
+                    eval('cell' + f.tileID + '.tileValue += 1');
+                }
+            }
+        }
+    }
 
     //log board
     for (var column in board) {
@@ -176,10 +175,14 @@ function startGame() {
         }
     }
 
-function onClick(cellID) {
-    console.log('The cell with ID ' + cellID + ' was clicked')
-    n = eval('cell' + cellID + '.tileID');
-    console.log(n);
-    document.getElementById(cellID).innerHTML = '0';
-    document.getElementById(cellID).style.backgroundColor = 'darkgray';
+    //runs the following when a tile is clicked
+    function onClick(cellID) {
+        console.log('The cell with ID ' + cellID + ' was clicked')
+        n = eval('cell' + cellID + '.tileID');
+        console.log(n);
+        document.getElementById(cellID).innerHTML = '0';
+        document.getElementById(cellID).style.backgroundColor = 'darkgray';
+    }
+
+//end the startGame() function
 }

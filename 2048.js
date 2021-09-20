8100a7abeda9f23819af54f0e7fc8de44d8f0c12
REDACTED <glitchgamer1459@gmail.com>
2021-09-20 13:53:04 -0700

    random tile generation in 2048.js

--- 2048.js
+++ 2048.js
@@ -1,3 +1,7 @@
+var rTileCoord = 0;
+var rTileValue = 0;
+var prevTileValue;
+
 //adds event listener to window object
 function load() {
     window.addEventListener('keydown', getKey, false);
@@ -20,20 +24,39 @@ function getKey(keyValue) {
 
 //runs the left Arrow code
 function left() {
+    randomTile()
     console.log('left');
 }
 
 //runs the right Arrow code
 function right() {
+    randomTile()
     console.log('right');
 }
 
 //runs the up Arrow code
 function up() {
+    randomTile()
     console.log('up');
 }
 
 //runs the down Arrow code
 function down() {
+    randomTile()
     console.log('down');
+}
+
+//generates a random tile when called
+function randomTile() {
+    rTileCoord = Math.floor(Math.random() * 16) + 1;
+    rTileValue = Math.floor(Math.random() * 4) + 1;
+    prevTileValue = document.getElementById(rTileCoord).innerHTML;
+    if (prevTileValue != '') {
+        randomTile();
+    }
+    if (rTileValue <= 3) {
+        document.getElementById(rTileCoord).innerHTML = 2;
+    } else if (rTileValue == 4) {
+        document.getElementById(rTileCoord).innerHTML = 4;
+    }
 }

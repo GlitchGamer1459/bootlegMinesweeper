be8af881511fd6c137aa27fd2f515fbc16119db2
REDACTED <glitchgamer1459@gmail.com>
2021-09-21 14:26:38 -0700

    finalized moveTiles() method in 2048.js

--- 2048.js
+++ 2048.js
@@ -2,6 +2,17 @@ var rTileCoord = 0;
 var rTileValue = 0;
 var prevTileValue;
 
+var r1s1;
+var r1s2;
+var r1s3;
+var r1s4;
+var filledTile;
+var carry = [];
+var index;
+var carryLength;
+var carryLengthMod;
+var counter;
+
 //adds event listener to window object and sets up board
 function load() {
     window.addEventListener('keydown', getKey, false);
@@ -25,129 +36,13 @@ function getKey(keyValue) {
 //runs the left Arrow code
 function left() {
     //row 1
-    for (var i = 1; i < 5; i++) {
-        currentCell = document.getElementById(i).innerHTML;
-        if (currentCell != '') {
-            if (document.getElementById(1).innerHTML == '') {
-                document.getElementById(1).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(2).innerHTML == '') {
-                document.getElementById(2).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(3).innerHTML == '') {
-                document.getElementById(3).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(4).innerHTML == '') {
-                document.getElementById(4).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            }
-        }
-    }
-    if (document.getElementById(1).innerHTML == document.getElementById(2).innerHTML && document.getElementById(1).innerHTML != '') {
-        document.getElementById(1).innerHTML = document.getElementById(1).innerHTML * 2;
-        document.getElementById(2).innerHTML = '';
-    }
-    if (document.getElementById(2).innerHTML == document.getElementById(3).innerHTML && document.getElementById(2).innerHTML != '') {
-        document.getElementById(2).innerHTML = document.getElementById(2).innerHTML * 2;
-        document.getElementById(3).innerHTML = '';
-    }
-    if (document.getElementById(3).innerHTML == document.getElementById(4).innerHTML && document.getElementById(3).innerHTML != '') {
-        document.getElementById(3).innerHTML = document.getElementById(3).innerHTML * 2;
-        document.getElementById(4).innerHTML = '';
-    }
+    moveTiles(0, 1, 2, 3, 4, 5);
     //row 2
-    for (var i = 5; i < 9; i++) {
-        currentCell = document.getElementById(i).innerHTML;
-        if (currentCell != '') {
-            if (document.getElementById(5).innerHTML == '') {
-                document.getElementById(5).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(6).innerHTML == '') {
-                document.getElementById(6).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(7).innerHTML == '') {
-                document.getElementById(7).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(8).innerHTML == '') {
-                document.getElementById(8).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            }
-        }
-    }
-    if (document.getElementById(5).innerHTML == document.getElementById(6).innerHTML && document.getElementById(5).innerHTML != '') {
-        document.getElementById(5).innerHTML = document.getElementById(5).innerHTML * 2;
-        document.getElementById(6).innerHTML = '';
-    }
-    if (document.getElementById(6).innerHTML == document.getElementById(7).innerHTML && document.getElementById(2).innerHTML != '') {
-        document.getElementById(6).innerHTML = document.getElementById(6).innerHTML * 2;
-        document.getElementById(7).innerHTML = '';
-    }
-    if (document.getElementById(7).innerHTML == document.getElementById(8).innerHTML && document.getElementById(3).innerHTML != '') {
-        document.getElementById(7).innerHTML = document.getElementById(7).innerHTML * 2;
-        document.getElementById(8).innerHTML = '';
-    }
+    moveTiles(4, 5, 6, 7, 8, 9);
     //row 3
-    for (var i = 9; i < 13; i++) {
-        currentCell = document.getElementById(i).innerHTML;
-        if (currentCell != '') {
-            if (document.getElementById(9).innerHTML == '') {
-                document.getElementById(9).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(10).innerHTML == '') {
-                document.getElementById(10).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(11).innerHTML == '') {
-                document.getElementById(11).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(12).innerHTML == '') {
-                document.getElementById(12).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            }
-        }
-    }
-    if (document.getElementById(9).innerHTML == document.getElementById(10).innerHTML && document.getElementById(5).innerHTML != '') {
-        document.getElementById(9).innerHTML = document.getElementById(9).innerHTML * 2;
-        document.getElementById(10).innerHTML = '';
-    }
-    if (document.getElementById(10).innerHTML == document.getElementById(11).innerHTML && document.getElementById(2).innerHTML != '') {
-        document.getElementById(10).innerHTML = document.getElementById(10).innerHTML * 2;
-        document.getElementById(11).innerHTML = '';
-    }
-    if (document.getElementById(11).innerHTML == document.getElementById(12).innerHTML && document.getElementById(3).innerHTML != '') {
-        document.getElementById(11).innerHTML = document.getElementById(11).innerHTML * 2;
-        document.getElementById(12).innerHTML = '';
-    }
+    moveTiles(8, 9, 10, 11, 12, 13);
     //row 4
-    for (var i = 13; i < 17; i++) {
-        currentCell = document.getElementById(i).innerHTML;
-        if (currentCell != '') {
-            if (document.getElementById(13).innerHTML == '') {
-                document.getElementById(13).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(14).innerHTML == '') {
-                document.getElementById(14).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(15).innerHTML == '') {
-                document.getElementById(15).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            } else if (document.getElementById(16).innerHTML == '') {
-                document.getElementById(16).innerHTML = currentCell;
-                document.getElementById(i).innerHTML = '';
-            }
-        }
-    }
-    if (document.getElementById(13).innerHTML == document.getElementById(14).innerHTML && document.getElementById(5).innerHTML != '') {
-        document.getElementById(13).innerHTML = document.getElementById(13).innerHTML * 2;
-        document.getElementById(14).innerHTML = '';
-    }
-    if (document.getElementById(14).innerHTML == document.getElementById(15).innerHTML && document.getElementById(2).innerHTML != '') {
-        document.getElementById(14).innerHTML = document.getElementById(14).innerHTML * 2;
-        document.getElementById(15).innerHTML = '';
-    }
-    if (document.getElementById(15).innerHTML == document.getElementById(16).innerHTML && document.getElementById(3).innerHTML != '') {
-        document.getElementById(15).innerHTML = document.getElementById(15).innerHTML * 2;
-        document.getElementById(16).innerHTML = '';
-    }
+    moveTiles(12, 13, 14, 15, 16, 17);
     //randomTile();
 }
 
@@ -182,4 +77,31 @@ function randomTile() {
     } else if (rTileValue == 4) {
         document.getElementById(rTileCoord).innerHTML = 4;
     }
+}
+
+//moves all parametered tiles to a specific direction
+function moveTiles(a, b, c, d, e, f) {
+    r1s1 = Number(document.getElementById(b).innerHTML);
+    r1s2 = Number(document.getElementById(c).innerHTML);
+    r1s3 = Number(document.getElementById(d).innerHTML);
+    r1s4 = Number(document.getElementById(e).innerHTML);
+    carry.push(r1s1, r1s2, r1s3, r1s4);
+    for (var i = b; i < f; i++) {
+        document.getElementById(i).innerHTML = '';
+    }
+    for (var i = a; i < e; i++) {
+        index = carry.indexOf(0);
+        if (carry[index] == 0) {
+            carry.splice(index, 1);
+        }
+    }
+    counter = 0;
+    console.log(carry);
+    carryLength = carry.length
+    carryLengthMod = b + carry.length;
+    for (var i = b; i < carryLengthMod; i++) {
+        document.getElementById(i).innerHTML = carry[counter];
+        counter = counter + 1;
+    }
+    carry = [];
 }
